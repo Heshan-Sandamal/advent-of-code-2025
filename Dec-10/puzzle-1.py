1e2685c231f954fd834095d95d27101be367bff3
Heshan <mhsandamal1@gmail.com>
2025-12-10 12:01:17 +0000

    Dec-10

---
+++ Dec-10/puzzle-1.py
@@ -0,0 +1,38 @@
+file = open("input.txt")
+lines = file.read().splitlines()
+
+import itertools
+
+ranges, ingredients = [], []
+ranges_input = True
+
+total = []
+for line in lines:
+    line_d = line.split(" ")
+    diagram = list(line_d[0])[1:-1]
+    buttons = []
+    for k in line_d[1:-1]:
+        buttons.append([(int(g)) for g in k[1:-1].split(",")])
+
+    min_p = 100000000000000000000000000
+    for x in range(1, len(buttons)):
+        pairs = itertools.combinations(buttons, x)
+
+        for p in pairs:
+            start = ["." for t in range(len(diagram))]
+            for p_array in p:
+                for i in p_array:
+                    if (start[i] == "."):
+                        start[i] = "#"
+                    elif (start[i] == "#"):
+                        start[i] = "."
+
+            if (start == diagram):
+                if (x < min_p):
+                    min_p = x
+                print("Presses", start, x, p)
+                break
+
+    total.append(min_p)
+
+print("Min presses", sum(total))
